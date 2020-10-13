e9baca6a2458b9de36a6031484a6e36bf154cbf9
SFMack <sfcamack@icloud.com>
2020-10-12 18:42:00 -0700

    added a readme file and the base template

---
+++ front-page.php
@@ -0,0 +1,105 @@
+<?php get_header(); ?>
+
+    <div id="banner">
+        <h1>&lt;BlackAnchor/&gt;</h1>
+        <h3>We guarentee we'll make your project a success</h3>
+    </div>
+
+    <!-- Main -->
+    <main>
+        <!-- Header (linked to all blogs) -->
+        <a href="blogslist.html">
+            <h2 class="section-heading">All Blogs</h2>
+        </a>
+
+        <!-- Section -->
+        <section>
+            <!-- card -->
+            <div class="card">
+                <div class="card-image">
+                    <a href="blogpost.html">
+                        <img src="img/image1.jpg" alt="card image">
+                    </a>
+                </div>
+
+                <div class="card-description">
+                    <a href="blogpost.html">
+                        <h3>The Blog Title Here</h3>
+                    </a>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed non velit cumque minima consectetur. Expedita reprehenderit placeat illo commodi mollitia id, impedit cupiditate adipisci nobis, quas officiis excepturi, eos iste?
+                    </p>
+                    <a href="#" class="btn-readmore">Read More</a>
+                </div>
+            </div> 
+            <!-- card -->
+            <div class="card">
+                <div class="card-image">
+                    <a href="blogpost.html">
+                        <img src="img/image2.jpg" alt="card image">
+                    </a>
+                </div>
+
+                <div class="card-description">
+                    <a href="#">
+                        <h3>The Blog Title Here</h3>
+                    </a>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed non velit cumque minima consectetur. Expedita reprehenderit placeat illo commodi mollitia id, impedit cupiditate adipisci nobis, quas officiis excepturi, eos iste?
+                    </p>
+                    <a href="blogpost.html" class="btn-readmore">Read More</a>
+                </div>
+            </div>
+        </section>
+
+        <h2 class="section-heading">All Projects</h2>
+
+        <!-- Section -->
+        <section>
+            <!-- card -->
+            <div class="card">
+                <div class="card-image">
+                    <a href="#">
+                        <img src="img/image3.jpg" alt="card image">
+                    </a>
+                </div>
+
+                <div class="card-description">
+                    <a href="#">
+                        <h3>The Project Title Here</h3>
+                    </a>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed non velit cumque minima consectetur. Expedita reprehenderit placeat illo commodi mollitia id, impedit cupiditate adipisci nobis, quas officiis excepturi, eos iste ?
+                    </p>
+                    <a href="#" class="btn-readmore">Read More</a>
+                </div>
+            </div>
+            <!-- card -->
+            <div class="card">
+                <div class="card-image">
+                    <a href="#">
+                        <img src="img/image4.jpg" alt="card image">
+                    </a>
+                </div>
+
+                <div class="card-description">
+                    <a href="#">
+                        <h3>The Project Title Here</h3>
+                    </a>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed non velit cumque minima consectetur. Expedita reprehenderit placeat illo commodi mollitia id, impedit cupiditate adipisci nobis, quas officiis excepturi, eos iste?
+                    </p>
+                    <a href="#" class="btn-readmore">Read More</a>
+                </div>
+            </div>
+        </section>
+
+        <h2 class="section-heading">Source Code</h2>
+        <section id="section-source">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero sequi delectus numquam magnam id alias eveniet voluptatem facere soluta nostrum blanditiis deserunt quia nisi tempore, repellendus maiores modi, temporibus quo!
+            </p>
+            <a href="#" class="btn-readmore">GitHub Profile</a>
+        </section>
+
+    <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -3,11 +3,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <?php wp_head(); ?>
-
+    <title>Black Anchor</title>
 </head>
 <body>
-    <header>
-        <h1>Header Here</h1>
-    </header>
+
+    <!-- Slideout Menu -->
+    <div id="slideout-menu">
+        <ul>
+            <li><a href="#">Home</a>
+            </li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">About</a></li>
+            <li>
+                <input type="text" placeholder="Search Here">
+            </li>
+        </ul>
+    </div>
+
+    <!-- Navigation -->
+    <nav>
+        <div id="logo-image">
+            <a href="#">
+                <img src="img/logo.png" alt="black anchor logo">
+            </a>
+        </div>
+
+        <div id="menu-icon">
+            <i class="fas fa-bars"></i>
+        </div>
+        <ul>
+            <li><a href="index.html" class="active">Home</a>
+            </li>
+            <li><a href="blogslist.html">Blog</a></li>
+            <li><a href="blogslist.html">Projects</a></li>
+            <li><a href="about.html">About</a></li>
+            <li>
+                <div id="search-icon">
+                    <i class="fas fa-search"></i>
+                </div>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- Search Box -->
+    <div id="searchbox">
+        <input type="text" placeholder="Search Here">
+    </div>
